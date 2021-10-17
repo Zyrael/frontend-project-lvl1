110c9e5af7553a940f2d796e4bd9b671ba96dd2a
Zyrael <etoyaleha@gmail.com>
2021-10-17 12:10:58 +0300

    fix isPrime function

--- games/prime.js
+++ games/prime.js
@@ -3,7 +3,7 @@ import start, * as game from '../src/index.js';
 const rulesMessage = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
-  for (let i = 2; i < Math.sqrt(num); i += 1) {
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) return false;
   }
   return num > 1;
