16b3a84c5882e51e2697f663e244e46cf90eee1d
Zyrael <etoyaleha@gmail.com>
2021-10-23 15:10:31 +0300

    refactor gcd game

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -2,23 +2,17 @@ import { start, getRandomInt } from '../index.js';
 
 const rulesMessage = 'Find the greatest common divisor of given numbers.';
 
-const findGcd = (a, b) => {
+const getCorrectAnswer = (a, b) => {
   if (!b) {
     return a;
   }
 
-  return findGcd(b, a % b);
-};
-
-const getCorrectAnswer = (question) => {
-  const [a, b] = question;
-
-  return findGcd(a, b);
+  return getCorrectAnswer(b, a % b);
 };
 
 const getQuestion = () => {
   const question = [getRandomInt(1, 100), getRandomInt(1, 100)];
-  const correctAnswer = `${getCorrectAnswer(question)}`;
+  const correctAnswer = `${getCorrectAnswer(...question)}`;
 
   return [question.join(' '), correctAnswer];
 };
